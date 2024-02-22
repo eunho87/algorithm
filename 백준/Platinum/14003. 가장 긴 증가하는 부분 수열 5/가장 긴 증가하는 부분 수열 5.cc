8153e03a06d528d1be6c8a9e4b41afa56d6a6457
eunho87 <129042671+eunho87@users.noreply.github.com>
2024-02-23 03:43:33 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 244 ms, Memory: 33144 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long arr[1000003];
+int arr2[1000003];
+int arr3[1000003];
+
+void Find(long long idx,long long num) {
+	if (idx == 0)	return;
+	if (arr2[idx] == num) {
+		Find(idx - 1, num - 1);
+		cout << arr3[idx] << " ";
+	}
+	else {
+		Find(idx - 1, num);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int n,q=0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr3[i];
+		int p = lower_bound(arr + 1, arr + 1 + q, arr3[i]) - arr;
+		arr[p] = arr3[i];
+		arr2[i] = p;
+		if (p == q + 1) q++;	
+
+	}
+	cout << q<<"\n";
+	Find(n,q);
+	return 0;
+}
