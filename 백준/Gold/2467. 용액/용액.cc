5fbd7602588c21942e8be36b385aaa5817f46df3
eunho87 <129042671+eunho87@users.noreply.github.com>
2024-02-23 02:44:32 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -1,54 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	ios::sync_with_stdio(false);
 
-	int N;
-	cin >> N;
-
-	vector<long long> tree(N);
-
-	for (int i = 0; i < N; i++)
-	{
+long long tree[100003];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+	long long n, ta, ans_a, ans_b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
 		cin >> tree[i];
 	}
-
-	int a = 0;
-	int b = N - 1;
-
-	long long Max, Min;
-
 	long long ans = LLONG_MAX;
-
-	while (a < b)
-	{
-		long long sum = tree[a] + tree[b];
-
-		if (abs(sum) < abs(ans))
-		{
-			Max = tree[b];
-			Min = tree[a];
-			ans = sum;
+	int a = 0, b = n - 1;
+	while (a < b) {
+		ta = tree[a] + tree[b];
+		if (ans > abs(ta)) {
+			ans_a = tree[a];
+			ans_b = tree[b];
+			ans = abs(ta);
 		}
 
-		if (sum < 0)
-		{
-			a+=1;
-		}
-
-		else if (sum > 0)
-		{
-			b-=1;
-		}
-
-		else
-		{
-			break;
-		}
+		if (ta > 0)	b -= 1;
+		else if (ta < 0)	a += 1;
+		else break;
 	}
-
-	cout << Min << ' ' << Max;
+	cout << ans_a << " " << ans_b;
+	return 0;
 }
