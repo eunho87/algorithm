3407eacf13b3b787304547c8fb3bdc9ed34fe869
eunho87 <129042671+eunho87@users.noreply.github.com>
2024-03-20 14:28:40 +0900

    [Silver IV] Title: 대피소, Time: 0 ms, Memory: 2024 KB, Score: 17 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.cc
+++ 백준/Silver/28215. 대피소/대피소.cc
@@ -14,7 +14,7 @@ int main() {
 	cin.tie(0);
 	int n, k;
 	int sum = INT_MAX;
-	int chk=0;
+	int chk=INT_MAX;
 	cin >> n >> k;
 
 	for (int i = 1; i <= n; ++i) {
@@ -27,17 +27,9 @@ int main() {
 		for (int j = 1; j <= n; j++) {
 			int cost = 0;
 			for (int p = 1; p <= n; p++) {
-				cost += abs(arr[j].x - arr[p].x) + abs(arr[j].y - arr[p].y);
+				cost= max(cost,abs(arr[j].x - arr[p].x) + abs(arr[j].y - arr[p].y));
 			}
-			if (sum > cost) {
-				sum = cost;
-				chk = 0;
-				for (int p = 1; p <= n; p++) {
-					int w;
-					w = abs(arr[j].x - arr[p].x) + abs(arr[j].y - arr[p].y);
-					if (chk < w)	chk = w;
-				}
-			}
+			chk= min (chk, cost);
 		}
 	}
 	if (k == 2) {
@@ -45,17 +37,9 @@ int main() {
 			for (int p = 1; p <= j - 1; p++) {
 				int cost = 0;
 				for (int q = 1; q <= n; q++) {
-					cost += min(abs(arr[j].x - arr[q].x) + abs(arr[j].y - arr[q].y), abs(arr[p].x - arr[q].x) + abs(arr[p].y - arr[q].y));
+					cost= max(abs(arr[j].x - arr[q].x) + abs(arr[j].y - arr[q].y), abs(arr[p].x - arr[q].x) + abs(arr[p].y - arr[q].y));
 				}
-				if (sum > cost) {
-					sum = cost;
-					chk = 0;
-					for (int q = 1; q <= n; q++) {
-						int w;
-						w= min(abs(arr[j].x - arr[q].x) + abs(arr[j].y - arr[q].y), abs(arr[p].x - arr[q].x) + abs(arr[p].y - arr[q].y));
-						if (chk < w)	chk = w;
-					}
-				}
+				chk = min(chk, cost);
 			}
 		}
 	}
@@ -65,17 +49,9 @@ int main() {
 				for (int q = 1; q <= p - 1; q++) {
 					int cost = 0;
 					for (int l = 1; l <= n; l++) {
-						cost += min(abs(arr[j].x - arr[l].x) + abs(arr[j].y - arr[l].y), min(abs(arr[p].x - arr[l].x) + abs(arr[p].y - arr[l].y), abs(arr[l].x - arr[q].x) + abs(arr[l].y - arr[q].y)));
+						cost += max(abs(arr[j].x - arr[l].x) + abs(arr[j].y - arr[l].y), max(abs(arr[p].x - arr[l].x) + abs(arr[p].y - arr[l].y), abs(arr[l].x - arr[q].x) + abs(arr[l].y - arr[q].y)));
 					}
-					if (sum > cost) {
-						sum = cost;
-						chk = 0;
-						for (int l = 1; l <= n; l++) {
-							int w;
-							w = min(abs(arr[j].x - arr[l].x) + abs(arr[j].y - arr[l].y), min(abs(arr[p].x - arr[l].x) + abs(arr[p].y - arr[l].y), abs(arr[l].x - arr[q].x) + abs(arr[l].y - arr[q].y)));
-							if (chk < w)	chk = w;
-						}
-					}
+					chk = min (chk, cost);
 				}
 
 			}
