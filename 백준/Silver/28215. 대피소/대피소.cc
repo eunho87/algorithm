c921293f61be68748de36f2cfdce9ac5ceb88073
eunho87 <129042671+eunho87@users.noreply.github.com>
2024-03-20 14:43:36 +0900

    [Silver IV] Title: 대피소, Time: 8 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.cc
+++ 백준/Silver/28215. 대피소/대피소.cc
@@ -13,7 +13,6 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	int n, k;
-	int sum = INT_MAX;
 	int chk=INT_MAX;
 	cin >> n >> k;
 
@@ -27,7 +26,7 @@ int main() {
 		for (int j = 1; j <= n; j++) {
 			int cost = 0;
 			for (int p = 1; p <= n; p++) {
-				cost= max(cost,abs(arr[j].x - arr[p].x) + abs(arr[j].y - arr[p].y));
+				cost= max (cost,abs(arr[j].x - arr[p].x) + abs(arr[j].y - arr[p].y));
 			}
 			chk= min (chk, cost);
 		}
@@ -37,7 +36,7 @@ int main() {
 			for (int p = 1; p <= j - 1; p++) {
 				int cost = 0;
 				for (int q = 1; q <= n; q++) {
-					cost= max(abs(arr[j].x - arr[q].x) + abs(arr[j].y - arr[q].y), abs(arr[p].x - arr[q].x) + abs(arr[p].y - arr[q].y));
+					cost= max(cost,min(abs(arr[j].x - arr[q].x) + abs(arr[j].y - arr[q].y), abs(arr[p].x - arr[q].x) + abs(arr[p].y - arr[q].y)));
 				}
 				chk = min(chk, cost);
 			}
@@ -49,7 +48,7 @@ int main() {
 				for (int q = 1; q <= p - 1; q++) {
 					int cost = 0;
 					for (int l = 1; l <= n; l++) {
-						cost += max(abs(arr[j].x - arr[l].x) + abs(arr[j].y - arr[l].y), max(abs(arr[p].x - arr[l].x) + abs(arr[p].y - arr[l].y), abs(arr[l].x - arr[q].x) + abs(arr[l].y - arr[q].y)));
+						cost = max(cost,min(abs(arr[j].x - arr[l].x) + abs(arr[j].y - arr[l].y), min(abs(arr[p].x - arr[l].x) + abs(arr[p].y - arr[l].y), abs(arr[l].x - arr[q].x) + abs(arr[l].y - arr[q].y))));
 					}
 					chk = min (chk, cost);
 				}
